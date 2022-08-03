71eebe4b775a819b1f120345fc8b54c96f766ca3
Yaqub Tawab Olamilekan <maktee2002@gmail.com>
2022-08-03 22:29:20 +0100

    vibes

---
+++ okay.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner; 
+import java.util.Arrays ;
+public class okay {
+public static void main (String[]args) {
+    String Tawab = "hello one";
+    int [] number = {3,5,6, 8};
+    Arrays.sort(number);
+    System.out.print(Arrays.toString(number));
+}
+}
